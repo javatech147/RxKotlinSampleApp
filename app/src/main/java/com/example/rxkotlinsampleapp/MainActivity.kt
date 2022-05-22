192f933f3eac4110a7c7d08487a06e6443ea6cb5
Manish Kumar <manishkumar@photon.in>
2022-05-22 15:39:42 +0530

    range()

--- app/src/main/java/com/example/rxkotlinsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/rxkotlinsampleapp/MainActivity.kt
@@ -11,16 +11,22 @@ import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 /*
-* fromIterable() operators will work on list type.
+* range() operators only works on Int types.
 * It will emit list items one by one.
-*     private val greetings = arrayListOf("Hello A", "Hello B", "Hello C")
-*     ...
-*     observable = Observable.fromIterable(greetings)
-*
+*     observable = Observable.range(0, 10)
+* Here it will emit 10 items starting from 0 means 0 to 9 items.
 * Log Output -
-* D/MainActivity: onNext: Hello A
-* D/MainActivity: onNext: Hello B
-* D/MainActivity: onNext: Hello C
+* D/MainActivity: onNext: 0
+* D/MainActivity: onNext: 1
+* D/MainActivity: onNext: 2
+* D/MainActivity: onNext: 3
+* D/MainActivity: onNext: 4
+* D/MainActivity: onNext: 5
+* D/MainActivity: onNext: 6
+* D/MainActivity: onNext: 7
+* D/MainActivity: onNext: 8
+* D/MainActivity: onNext: 9
+* D/MainActivity: onComplete:
 * */
 
 class MainActivity : AppCompatActivity() {
@@ -28,11 +34,9 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
 
-    private val greetings = arrayListOf("Hello A", "Hello B", "Hello C")
+    private lateinit var observable: Observable<Int>
 
-    private lateinit var observable: Observable<String>
-
-    private lateinit var disposableObserver: DisposableObserver<String>
+    private lateinit var disposableObserver: DisposableObserver<Int>
 
     private val compositeDisposable = CompositeDisposable()
 
@@ -41,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        observable = Observable.fromIterable(greetings)
+        observable = Observable.range(0, 10)
 
         compositeDisposable.add(
             observable.subscribeOn(Schedulers.io())
@@ -50,15 +54,17 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun getObserver(): DisposableObserver<String> {
-        disposableObserver = object : DisposableObserver<String>() {
-            override fun onNext(str: String?) {
-                Log.d(TAG, "onNext: $str")
+    private fun getObserver(): DisposableObserver<Int> {
+        disposableObserver = object : DisposableObserver<Int>() {
+            override fun onNext(item: Int) {
+                Log.d(TAG, "onNext: $item")
             }
 
             override fun onError(e: Throwable?) = Unit
 
-            override fun onComplete() = Unit
+            override fun onComplete() {
+                Log.d(TAG, "onComplete: ")
+            }
         }
         return disposableObserver
     }
